e3b42e3fa6fa241b86c4ebf4273e62f522db0a47
AnujaySaraf <AnujaySaraf@users.noreply.github.com>
2017-06-17 02:26:05 +0000

    Done

--- build.py
+++ build.py
@@ -3,5 +3,10 @@ import pandas as pd
 
 def solution(array):
     """
-    Enter your code here
+    Series 2 panda
     """
+    pandaArray = pd.Series(array)
+    print pandaArray
+    return pandaArray
+
+solution([2, 4, 6, 8, 10])
